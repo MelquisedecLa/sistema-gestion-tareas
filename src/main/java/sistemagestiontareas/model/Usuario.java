f18bca825b7db6f1128088b79cc0eee015e176db
Andres-Ban04 <00044125@uca.edu.sv>
2026-04-09 11:28:49 -0600

    feat: creacion de usuarios

--- src/main/java/sistemagestiontareas/model/Usuario.java
+++ src/main/java/sistemagestiontareas/model/Usuario.java
@@ -1,4 +1,54 @@
 package sistemagestiontareas.model;
 
 public class Usuario {
+
+
+    private int id;
+    private String nombre;
+    private String email;
+    private String password;
+
+    public Usuario() {}
+
+    public Usuario(String nombre, int id, String email, String password) {
+        this.nombre = nombre;
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    //public abstract void crearElemento();
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
--- src/main/java/sistemagestiontareas/model/UsuarioPremium.java
+++ src/main/java/sistemagestiontareas/model/UsuarioPremium.java
@@ -1,4 +1,18 @@
 package sistemagestiontareas.model;
 
-public class UsuarioPremium {
+public class UsuarioPremium extends Usuario {
+
+    public UsuarioPremium(String nombre, int id, String email, String password) {
+        super(nombre, id , email, password);
+    }
+
+    public void accesoCompleto() {
+        System.out.println("Acceso completo habilitado.");
+    }
+
+    //@Override
+    public void crearElemento() {
+        System.out.println("Usuario premium crea elemento sin restricciones.");
+    }
+//}
 }
