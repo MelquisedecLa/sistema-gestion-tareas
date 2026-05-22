eb0ab3554136d0dc3ccff3d475b59cb59991cd0d
DanielAHB1 <00036225@uca.edu.sv>
2026-05-22 10:54:48 -0600

    feat: Implementar Autoguardado con uso de hilos

---
+++ src/main/java/sistemagestiontareas/thread/AutoGuardarThread.java
@@ -0,0 +1,36 @@
+package sistemagestiontareas.thread;
+
+public class AutoGuardarThread extends Thread {
+    private boolean ejecutando = true;
+    private String tipoElemento;
+    private int segundos = 0;
+
+    public AutoGuardarThread(String tipoElemento) {
+        this.tipoElemento = tipoElemento;
+    }
+
+    @Override
+    public void run() {
+        while (ejecutando) {
+            try {
+                Thread.sleep(1000);
+                segundos++;
+
+                if (segundos % 30 == 0) {
+
+                    System.out.print("\r");
+                    System.out.print(" [Auto-Guardado] " + tipoElemento + " guardado automáticamente...");
+                    System.out.print("\n\r");
+                    System.out.print("Puedes continuar editando el campo anterior: ");
+                }
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
+
+    public void detener() {
+        ejecutando = false;
+        this.interrupt();
+    }
+}
